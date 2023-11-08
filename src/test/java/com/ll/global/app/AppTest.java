5449ed339b11a4db62a50cc34e386ab764778137
schim613 <uximin@naver.com>
2023-11-08 11:53:56 +0900

    t1, pass

--- src/test/java/com/ll/global/app/AppTest.java
+++ src/test/java/com/ll/global/app/AppTest.java
@@ -10,11 +10,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest {
     @Test
-    @DisplayName("프로그램 시작 시 \"== 명언앱 ==\" 출력")
+    @DisplayName("프로그램 시작 시 \"== 명언 앱 ==\" 출력")
     void t1() {
         ByteArrayOutputStream byteArrayOutputStream = TestUtil.setOutToByteArray();
 
-        new App();
+        new App().run();
 
         String out = byteArrayOutputStream.toString().trim();
         TestUtil.clearSetOutToByteArray(byteArrayOutputStream);
