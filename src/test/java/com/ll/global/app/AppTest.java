9e65cd122242b82610b840c0ca4e581c80921cd1
schim613 <uximin@naver.com>
2023-11-08 11:51:43 +0900

    t1

---
+++ src/test/java/com/ll/global/app/AppTest.java
@@ -0,0 +1,25 @@
+package com.ll.global.app;
+
+import com.ll.standard.util.TestUtil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest {
+    @Test
+    @DisplayName("프로그램 시작 시 \"== 명언앱 ==\" 출력")
+    void t1() {
+        ByteArrayOutputStream byteArrayOutputStream = TestUtil.setOutToByteArray();
+
+        new App();
+
+        String out = byteArrayOutputStream.toString().trim();
+        TestUtil.clearSetOutToByteArray(byteArrayOutputStream);
+
+        assertThat(out)
+                .contains("== 명언 앱 ==");
+    }
+}
