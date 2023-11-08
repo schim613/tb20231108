7468aab509756ecba92a9310ef586e379baf7271
schim613 <uximin@naver.com>
2023-11-08 15:27:13 +0900

    t2

--- src/test/java/com/ll/global/app/AppTest.java
+++ src/test/java/com/ll/global/app/AppTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
+import java.util.Scanner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,14 +13,34 @@ public class AppTest {
     @Test
     @DisplayName("프로그램 시작 시 \"== 명언 앱 ==\" 출력")
     void t1() {
+        Scanner scanner = TestUtil.genScanner("""
+                """.stripIndent()); // t2에서 돌아가야하기 때문에 scanner 추가
+
         ByteArrayOutputStream byteArrayOutputStream = TestUtil.setOutToByteArray();
 
-        new App().run();
+        new App(scanner).run();
 
         String out = byteArrayOutputStream.toString().trim();
         TestUtil.clearSetOutToByteArray(byteArrayOutputStream);
 
         assertThat(out)
                 .contains("== 명언 앱 ==");
+    }
+
+    @Test
+    @DisplayName("종료")
+    void t2() {
+        Scanner scanner = TestUtil.genScanner("""
+                종료
+                """.stripIndent());
+
+        ByteArrayOutputStream byteArrayOutputStream = TestUtil.setOutToByteArray();
+
+        new App(scanner).run();
+
+        String out = byteArrayOutputStream.toString().trim();
+        TestUtil.clearSetOutToByteArray(byteArrayOutputStream);
+
+
     }
 }
