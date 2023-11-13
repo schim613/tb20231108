d56ce5134fa1390616bed584cf998559d2e0e25f
schim613 <uximin@naver.com>
2023-11-13 11:45:41 +0900

    t7, pass

---
+++ src/main/java/com/ll/domain/quotation/quotation/entity/Quotation.java
@@ -0,0 +1,12 @@
+package com.ll.domain.quotation.quotation.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Quotation {
+    private final long id;
+    private final String authorName;
+    private final String content;
+}
