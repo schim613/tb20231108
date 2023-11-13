30de96ee0a92b85d5aef7f31110977972fc220a3
schim613 <uximin@naver.com>
2023-11-13 13:31:16 +0900

    t9, refactoring

--- src/main/java/com/ll/global/app/App.java
+++ src/main/java/com/ll/global/app/App.java
@@ -23,19 +23,20 @@ public class App {
         while (true) {
             final String cmd = scanner.nextLine().trim();
 
+            final String[] cmdBits = cmd.split("\\?", 2);
+            final String action = cmdBits[0].trim();
+            final String queryString = cmdBits.length == 2 ? cmdBits[1].trim() : "";
 
-            switch (cmd) {
-                case "삭제?id=1" -> {
+
+            switch (action) {
+                case "삭제" -> {
+                    final String idStr = queryString.replace("id=", "");
+                    final long id = Long.parseLong(idStr);
+
                     quotations
-                            .removeIf(quotation -> quotation.getId() == 1);
+                            .removeIf(quotation -> quotation.getId() == id);
 
                     System.out.println("1번 명언이 삭제되었습니다.");
-                }
-                case "삭제?id=2" -> {
-                    quotations
-                            .removeIf(quotation -> quotation.getId() == 2);
-
-                    System.out.println("2번 명언이 삭제되었습니다.");
                 }
                 case "등록" -> {
                     System.out.print("명언 : ");
